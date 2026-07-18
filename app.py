5e892d569263c4b21e569c1e5a9ed6a2c77c9594
JuanTorresss <143672405+JuanTorresss@users.noreply.github.com>
2026-07-18 08:25:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ cedula = st.text_input(
 
 if st.button("Consultar"):
 
-    col1, col2, col3 = st.columns([1,1,1])
+    col1, col2, col3 = st.columns([1,2,1])
 
     with col2:
         st.image("3148295.png", width=80)
